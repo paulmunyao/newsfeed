5b594f02ae6d15c42cf1ae60d23866b59cea9e94
paulmunyao <paulmunyao094@gmail.com>
2022-07-24 23:08:08 +0300

    Add button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ItemsComponent
+    ItemsComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
