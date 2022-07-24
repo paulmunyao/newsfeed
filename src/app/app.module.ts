09ac2dfcef38dce1c27cead7191b8fa5886afe09
paulmunyao <paulmunyao094@gmail.com>
2022-07-24 23:54:18 +0300

    Generate new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { ButtonComponent } from './button/button.component';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    ButtonComponent
+    ButtonComponent,
+    ItemListComponent
   ],
   imports: [
     BrowserModule,
