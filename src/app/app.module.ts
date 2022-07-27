887440f3614c2e91a1e91af172b40593d6ee1f7b
paulmunyao <paulmunyao094@gmail.com>
2022-07-27 23:41:34 +0300

    add new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
