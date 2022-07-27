c5cfc1102639b4fe5cc4e59ba076ab86df2292e3
paulmunyao <paulmunyao094@gmail.com>
2022-07-27 22:44:21 +0300

    remove unused code form the code path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppModule } from './app/app.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
