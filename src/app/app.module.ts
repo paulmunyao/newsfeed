9ec308da99f2fb84135540c8b9dd8db0875adf45
paulmunyao <paulmunyao094@gmail.com>
2022-07-27 22:40:28 +0300

    import app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ApModule } from './app/app.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,4 +22,5 @@ import { UserComponent } from './user/user.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+// export class AppModule { }
