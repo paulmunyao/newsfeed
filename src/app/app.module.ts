447cdab27d7a998c148b78540db168303a0ab6f5
paulmunyao <paulmunyao094@gmail.com>
2022-07-27 22:41:19 +0300

    edit code path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ApModule } from './app.module';
+import { AppModule } from './app.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
