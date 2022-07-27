e2657bcd015f6a7c3e2bcaa2b0f288232acb5533
paulmunyao <paulmunyao094@gmail.com>
2022-07-28 00:22:47 +0300

    indent code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +8,9 @@ import { ItemsComponent } from './items/items.component';
 import { ButtonComponent } from './button/button.component';
 import { UserComponent } from './user/user.component';
 
-
 @NgModule({
   declarations: [AppComponent, ItemsComponent, ButtonComponent, UserComponent],
-  imports: [BrowserModule, AppRoutingModule,MatButtonModule],
-  
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
