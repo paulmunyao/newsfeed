b7ae793fb31696322b60802707579e95e1afd420
paulmunyao <paulmunyao094@gmail.com>
2022-08-01 23:19:16 +0300

    modify code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   msg!: string;
-  // isDisplay = false;
+  isDisplay = false;
 
   constructor() {}
 
