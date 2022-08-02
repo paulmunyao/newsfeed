92868b3e603bc5b49290673c68cbf8345b0da54f
paulmunyao <paulmunyao094@gmail.com>
2022-08-02 23:02:38 +0300

    indent code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -14,11 +14,11 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {}
 
   toggleDisplay() {
-    var x = document.getElementById("myDiv");
-    if (x.innerHTML==="Friend1"){
-      x.innerHTML="swap";
-    }else{
-      x.innerHTML="Friend1"
+    var x = document.getElementById('myDiv');
+    if (x.innerHTML === 'Friend1') {
+      x.innerHTML = 'swap';
+    } else {
+      x.innerHTML = 'Friend1';
     }
     this.msg = 'Friend1 liked a photo of friend2';
     this.isDisplay = !this.isDisplay;
