1c22114b6d949cbe22b1d384910a5cb2dc0f8729
paulmunyao <paulmunyao094@gmail.com>
2022-08-02 23:04:11 +0300

    edit code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,7 +15,7 @@ export class UserComponent implements OnInit {
 
   toggleDisplay() {
     var x = document.getElementById('myDiv');
-    if (x.innerHTML === 'Friend1') {
+    if (x!.innerHTML === 'Friend1') {
       x.innerHTML = 'swap';
     } else {
       x.innerHTML = 'Friend1';
