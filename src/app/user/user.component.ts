8ce1d5d01cb15c034267368a10a6302e6338af11
paulmunyao <paulmunyao094@gmail.com>
2022-08-03 00:55:09 +0300

    edit code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -19,7 +19,7 @@ export class UserComponent implements OnInit {
   }
 
   myFunction() {
-    var x = document.getElementById('myDiv');
+    // var x = document.getElementById('myDiv');
     if (x!.innerHTML === 'Friend1') {
       x!.innerHTML = 'ojjjj';
     } else {
