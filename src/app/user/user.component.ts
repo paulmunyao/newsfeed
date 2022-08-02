92530208c0b72e9255d415fcf7096344309ea31e
paulmunyao <paulmunyao094@gmail.com>
2022-08-03 00:51:43 +0300

    comment unused code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -14,12 +14,12 @@ export class UserComponent implements OnInit {
   ngOnInit(): void {}
 
   toggleDisplay() {
-    var x = document.getElementById('myDiv');
-    if (x!.innerHTML === 'Friend1') {
-      x!.innerHTML = 'oojdsdiwd';
-    } else {
-      x!.innerHTML = 'Friend1';
-    }
+    // var x = document.getElementById('myDiv');
+    // if (x!.innerHTML === 'Friend1') {
+    //   x!.innerHTML = 'oojdsdiwd';
+    // } else {
+    //   x!.innerHTML = 'Friend1';
+    // }
     this.msg = 'Friend1 liked a photo of friend2';
     this.isDisplay = !this.isDisplay;
 
