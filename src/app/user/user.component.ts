b1098bd8ee6dacbd4dd44e988ff08e851dbf0d31
paulmunyao <paulmunyao094@gmail.com>
2022-08-03 23:17:25 +0300

    remove unused code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   public show:boolean = false;
   public msg:any = 'Show';
-  // msg!: string;
+  ;
   isDisplay = true;
 
   constructor() {}
@@ -30,7 +30,7 @@ export class UserComponent implements OnInit {
 
   toggleShow(){
     this.isShown = !this.isShown;
-    if(this.isShown) 
+    if(this.isShown)
     this.msg = "Hide";
   else
     this.msg = "Show";
