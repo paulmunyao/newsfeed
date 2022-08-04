29d28d717e3e81c3677b073ed4c4a644ca8267b0
paulmunyao <paulmunyao094@gmail.com>
2022-08-05 00:23:11 +0300

    return else statement

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -38,6 +38,6 @@ export class UserComponent implements OnInit {
   toggleNotification() {
     this.shown = !this.shown;
     if(this.shown) this.msg = 'Paul';
-    
+    else this.msg = 'Pa';
   }
 }
