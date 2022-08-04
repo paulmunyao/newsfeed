e7c6c491706c69f740370f2e92a7cbac02685cfd
paulmunyao <paulmunyao094@gmail.com>
2022-08-05 00:18:27 +0300

    edit message

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
-  
+  msg!: string;
   public show: boolean = false;
   isDisplay = true;
   public isShown: boolean = false;
