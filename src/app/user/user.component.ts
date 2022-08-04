6027546441e0eb883107ab0aad58ce7e31ebb732
paulmunyao <paulmunyao094@gmail.com>
2022-08-05 00:21:28 +0300

    edit code

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   msg!: string;
   isDisplay = true;
-  isToggle!: true;
+  // isToggle!: true;
   public show: boolean = false;
   public isShown: boolean = false;
   
