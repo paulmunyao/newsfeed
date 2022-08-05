0ee08d3d0cbb02cfff88d4af43674811c3bc951c
paulmunyao <paulmunyao094@gmail.com>
2022-08-06 00:41:13 +0300

    remove conditional statement on the first statement of show toggle

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -20,8 +20,8 @@ export class UserComponent implements OnInit {
 
   toggleDisplay() {
     this.showDisplay = !this.showDisplay;
-    if (this.showDisplay) this.msg = ""
-    else this.msg = ""
+    // if (this.showDisplay) this.msg = ""
+    // else this.msg = ""
   }
 
   toggle() {
